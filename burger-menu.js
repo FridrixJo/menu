4eb9ee2b87486399f893c5dac1d095eb40db32f9
pavel <pglutov@gmail.com>
2020-06-12 17:45:42 +0300

    2level

--- burger-menu.js
+++ burger-menu.js
@@ -1,5 +1,7 @@
 let btn = document.querySelector('.menu-btn');
+    block = document.querySelector('.menu-nav');
     btn.addEventListener('click', (e) => {
         e.preventDefault();
         btn.classList.toggle('menu-btn_active');
-    })
+        block.classList.toggle('menu-nav__active');
+    });
